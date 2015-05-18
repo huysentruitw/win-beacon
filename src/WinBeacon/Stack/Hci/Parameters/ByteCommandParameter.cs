1a077ecb832b7045e5e74a298bb6f3f0614483a1
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2015-05-18 23:13:07 +0200

    Add command parameter classes

---
+++ src/WinBeacon/Stack/Hci/Parameters/ByteCommandParameter.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Copyright 2015 Huysentruit Wouter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace WinBeacon.Stack.Hci.Parameters
+{
+    internal class ByteCommandParameter : ICommandParameter
+    {
+        public byte Value { get; private set; }
+
+        public ByteCommandParameter(byte value)
+        {
+            Value = value;
+        }
+
+        public byte[] ToByteArray()
+        {
+            return new byte[] { Value };
+        }
+    }
+}
