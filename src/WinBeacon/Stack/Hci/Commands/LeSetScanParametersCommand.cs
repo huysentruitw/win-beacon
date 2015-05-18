8d702815f1151578ad69c1cb36aa16039f214059
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2015-05-18 23:14:39 +0200

    Add command classes

---
+++ src/WinBeacon/Stack/Hci/Commands/LeSetScanParametersCommand.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Copyright 2015 Huysentruit Wouter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Diagnostics;
+using WinBeacon.Stack.Hci.Parameters;
+
+namespace WinBeacon.Stack.Hci.Commands
+{
+    internal class LeSetScanParametersCommand : Command
+    {
+        public LeSetScanParametersCommand(bool activeScanning, ushort intervalInMs, ushort windowInMs, bool useRandomAddress, bool onlyAcceptWhitelistedAdvertisers)
+            : base(OpcodeGroup.LeController, 0x0B)
+        {
+            ushort intervalCode = (ushort)Math.Ceiling(intervalInMs * 0.625);
+            ushort windowCode = (ushort)Math.Ceiling(windowInMs * 0.625);
+            Debug.Assert(intervalCode >= 0x0004);
+            Debug.Assert(intervalCode <= 0x4000);
+            Debug.Assert(windowCode >= 0x0004);
+            Debug.Assert(windowCode <= 0x4000);
+            Parameters.Add(new BoolCommandParameter(activeScanning));
+            Parameters.Add(new UshortCommandParameter(intervalCode));
+            Parameters.Add(new UshortCommandParameter(windowCode));
+            Parameters.Add(new BoolCommandParameter(useRandomAddress));
+            Parameters.Add(new BoolCommandParameter(onlyAcceptWhitelistedAdvertisers));
+        }
+    }
+}
