3ee0470acfe1b95c0b2906809b30928398e0b5ba
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2015-05-28 21:11:19 +0200

    Refactor Beacon code

--- src/WinBeacon/Beacon.cs
+++ src/WinBeacon/Beacon.cs
@@ -15,6 +15,9 @@
  */
 
 using System;
+using System.Collections.Generic;
+using WinBeacon.Stack;
+using WinBeacon.Stack.Hci.Events;
 
 namespace WinBeacon
 {
@@ -23,62 +26,62 @@ namespace WinBeacon
     /// </summary>
     public class Beacon : IComparable
     {
-        /// <summary>
-        /// When true, RangeInMeters will be calculated using an approximation of the iOS range calculation
-        /// instead of a simple linear ratio calculation. Default is true.
-        /// </summary>
-        public static bool UseApproximateIosRangeCalculation = true;
-
+        private const int AppleCompanyId = 0x4C00;
         /// <summary>
         /// UUID of the beacon.
         /// </summary>
-        public string Uuid { get; internal set; }
+        public string Uuid { get; private set; }
         /// <summary>
         /// Bluetooth MAC-address of the beacon.
         /// </summary>
-        public byte[] Address { get; internal set; }
+        public byte[] Address { get; private set; }
         /// <summary>
         /// Major number of the beacon.
         /// </summary>
-        public ushort Major { get; internal set; }
+        public int Major { get; private set; }
         /// <summary>
         /// Minor number of the beacon.
         /// </summary>
-        public ushort Minor { get; internal set; }
+        public int Minor { get; private set; }
         /// <summary>
         /// RSSI power of the beacon in dB.
         /// </summary>
-        public sbyte Rssi { get; internal set; }
+        public int Rssi { get; private set; }
         /// <summary>
         /// Calibrated TX power of the beacon in dB.
         /// </summary>
-        public sbyte CalibratedTxPower { get; internal set; }
+        public int CalibratedTxPower { get; private set; }
+        /// <summary>
+        /// CompanyId of the beacon (0x4C00 for Apple iBeacon).
+        /// </summary>
+        public int CompanyId { get; private set; }
         /// <summary>
         /// True if the beacon is an (emulation of) Apple iBeacon.
         /// </summary>
-        public bool IsAppleIBeacon { get; internal set; }
-
-        /// <summary>
-        /// The estimated range in meters.
-        /// </summary>
-        public double RangeInMeters
+        public bool IsAppleIBeacon
         {
-            get
-            {
-                return UseApproximateIosRangeCalculation
-                    ? BeaconHelpers.GetApproximateIosRange(Rssi, CalibratedTxPower)
-                    : BeaconHelpers.GetRange(Rssi, CalibratedTxPower);
-            }
+            get { return CompanyId == AppleCompanyId; }
+        }
+
+        private Beacon()
+        {
         }
 
         /// <summary>
-        /// Returns a string representation of the beacon data.
+        /// Creates a beacon.
         /// </summary>
-        /// <returns>String representation of the beacon data.</returns>
-        public override string ToString()
+        /// <param name="uuid">UUID of the beacon.</param>
+        /// <param name="major">Major number of the beacon.</param>
+        /// <param name="minor">Minor number of the beacon.</param>
+        /// <param name="companyId">CompanyId, defaults to comapny id of Apple (0x4C00).</param>
+        /// <param name="calibratedTxPower">Calibrated TX power of the beacon in dB.</param>
+        public Beacon(string uuid, int major, int minor, int calibratedTxPower, int companyId = AppleCompanyId)
         {
-            return string.Format("UUID: {0}, Address: {1}, Major: {2}, Minor: {3}, Range: {4:0.0}m, RSSI: {5}, TxPower: {6}dB, IsAppleIBeacon: {7}",
-                Uuid, BitConverter.ToString(Address), Major, Minor, RangeInMeters, Rssi, CalibratedTxPower, IsAppleIBeacon);
+            Uuid = uuid;
+            Major = major;
+            Minor = minor;
+            CalibratedTxPower = calibratedTxPower;
+            CompanyId = companyId;
         }
 
         /// <summary>
@@ -94,6 +97,81 @@ namespace WinBeacon
             return Uuid.NullableCompareTo(other.Uuid)
                 ?? Major.NullableCompareTo(other.Major)
                 ?? Minor.CompareTo(other.Minor);
+        }
+
+        /// <summary>
+        /// Returns a string representation of the beacon data.
+        /// </summary>
+        /// <returns>String representation of the beacon data.</returns>
+        public override string ToString()
+        {
+            return string.Format("UUID: {0}, Address: {1}, Major: {2}, Minor: {3}, RSSI: {4}, TxPower: {5}dB, CompanyId: 0x{6:X}, Distance: {7}m",
+                Uuid, BitConverter.ToString(Address), Major, Minor, Rssi, CalibratedTxPower, CompanyId, this.GetRange());
+        }
+
+        /// <summary>
+        /// Parse Scan Response event to a beacon instance.
+        /// </summary>
+        /// <param name="e">The event.</param>
+        /// <returns>The beacon or null in case of failure.</returns>
+        internal static Beacon Parse(LeAdvertisingEvent e)
+        {
+            if (e.EventType == LeAdvertisingEventType.ScanRsp)
+                throw new ArgumentException("Not a Scan Response event", "e");
+            if (e.Payload.Length < 9)
+                return null;
+            var payload = new Queue<byte>(e.Payload);
+            var ad1Length = payload.Dequeue();
+            var ad1Type = payload.Dequeue();
+            var flags = payload.Dequeue();
+            var ad2Length = payload.Dequeue();
+            var ad2Type = payload.Dequeue();
+            var companyId = (ushort)((payload.Dequeue() << 8) + payload.Dequeue());
+            var b0advInd = payload.Dequeue();
+            var b1advInd = payload.Dequeue();
+            if (ad1Length != 2 || ad1Type != 0x01 || ad2Length < 26 || ad2Type != 0xFF)
+                return null;
+            var uuid = payload.Dequeue(16);
+            var major = (ushort)((payload.Dequeue() << 8) + payload.Dequeue());
+            var minor = (ushort)((payload.Dequeue() << 8) + payload.Dequeue());
+            var txPower = (sbyte)payload.Dequeue();
+            return new Beacon
+            {
+                Uuid = uuid.ToLittleEndianFormattedUuidString(),
+                Address = e.Address,
+                Major = major,
+                Minor = minor,
+                Rssi = e.Rssi,
+                CalibratedTxPower = txPower,
+                CompanyId = companyId
+            };
+        }
+
+        internal byte[] ToAdvertisingData()
+        {
+            var result = new List<byte>();
+            result.AddRange(new byte[]
+            {
+                0x02,   // ad1Length
+                0x01,   // ad1Type
+                0x04,   // flags
+                0x1B,   // ad2Length
+                0xFF,   // ad2Type
+                (byte)(CompanyId >> 8),
+                (byte)(CompanyId & 0xFF),
+                0x02,   // b0advInd
+                0x15    // b1advInd
+            });
+            result.AddRange(Uuid.FromLittleEndianFormattedUuidString());
+            result.AddRange(new byte[]
+            {
+                (byte)(Major >> 8),
+                (byte)(Major & 0xFF),
+                (byte)(Minor >> 8),
+                (byte)(Minor & 0xFF),
+                (byte)CalibratedTxPower
+            });
+            return result.ToArray();
         }
     }
 }
---
+++ src/WinBeacon/Extensions/BeaconExtensionMethods.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Copyright 2015 Huysentruit Wouter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WinBeacon
+{
+    /// <summary>
+    /// Beacon extension methods.
+    /// </summary>
+    public static class BeaconExtensionMethods
+    {
+        /// <summary>
+        /// Gets the calculated range in meters.
+        /// </summary>
+        /// <param name="beacon">The beacon instance.</param>
+        /// <returns>The calculated range in meters.</returns>
+        public static double GetRange(this Beacon beacon)
+        {
+            double dbRatio = beacon.CalibratedTxPower - beacon.Rssi;
+            double linearRatio = Math.Pow(10, dbRatio / 10);
+            return Math.Sqrt(linearRatio);
+        }
+
+        /// <summary>
+        /// Gets the calculated range in meters using a curve that approximates the iOS ranging.
+        /// </summary>
+        /// <param name="beacon">The beacon instance.</param>
+        /// <returns>The calculated range in meters.</returns>
+        public static double GetApproximateIosRange(this Beacon beacon)
+        {
+            if (beacon.Rssi == 0)
+                return -1;
+            var ratio = (double)beacon.Rssi / (double)beacon.CalibratedTxPower;
+            if (ratio < 1.0)
+                return Math.Pow(ratio, 10);
+            return 0.89979 * Math.Pow(ratio, 7.7095) + 0.111;
+        }
+
+        internal static string ToLittleEndianFormattedUuidString(this byte[] uuid)
+        {
+            var digits = new Queue<char>(BitConverter.ToString(uuid).Replace("-", "").ToLower());
+            return new string((from c in "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" select c == '-' ? '-' : digits.Dequeue()).ToArray());
+        }
+
+        internal static byte[] FromLittleEndianFormattedUuidString(this string uuid)
+        {
+            if (!Regex.IsMatch(uuid, @"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", RegexOptions.IgnoreCase))
+                throw new FormatException("Incorrect uuid format");
+            return (from Match m in Regex.Matches(uuid, @"[0-9a-f]{2}", RegexOptions.IgnoreCase) select Convert.ToByte(m.Value, 16)).ToArray();
+        }
+    }
+}
