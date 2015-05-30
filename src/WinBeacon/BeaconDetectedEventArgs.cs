c40fd07b3f0b8627e757fb84b4a45355cfc5271c
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2015-05-30 09:40:34 +0200

    Add BeaconDetectedEventArgs

---
+++ src/WinBeacon/BeaconDetectedEventArgs.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Copyright 2015 Huysentruit Wouter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+
+namespace WinBeacon
+{
+    /// <summary>
+    /// Event arguments for the BeaconDetected event.
+    /// </summary>
+    public class BeaconDetectedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the detected beacon.
+        /// </summary>
+        public Beacon Beacon { get; private set; }
+
+        internal BeaconDetectedEventArgs(Beacon beacon)
+        {
+            Beacon = beacon;
+        }
+    }
+}
