ebba9988b1fe199cb044c874554d57cedbca7453
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2015-05-30 09:47:41 +0200

    Add sample console

---
+++ samples/SampleConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WinBeacon;
+
+namespace SampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var hub = new BeaconHub(0x050D, 0x065A))
+            {
+                hub.BeaconDetected += (sender, e) => Console.WriteLine(e.Beacon.ToString());
+
+                hub.EnableAdvertising(new Beacon("", 1, 135, -52));
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
