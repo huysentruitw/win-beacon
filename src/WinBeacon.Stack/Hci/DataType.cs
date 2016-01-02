a86f96eb23bdaaa2a1bc9ec82ac8ae66f5b4ac97
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2016-01-02 15:46:21 +0100

    Update copyright year

--- src/WinBeacon.Stack/Hci/DataType.cs
+++ src/WinBeacon.Stack/Hci/DataType.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2015 Huysentruit Wouter
+ * Copyright 2015-2016 Huysentruit Wouter
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
