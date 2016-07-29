461d88732b7676aba72c098488f4eacbb812c7e7
TheScribbler <stefan.magerstedt@khs.com>
2016-07-29 15:25:48 +0200

    LeSetAdvertisingParameters.cs changed with new enums and correct parameters

--- src/WinBeacon.Stack/Hci/Commands/LeSetAdvertisingParameters.cs
+++ src/WinBeacon.Stack/Hci/Commands/LeSetAdvertisingParameters.cs
@@ -15,19 +15,50 @@
  */
 
 using System;
+using System.Diagnostics;
 using WinBeacon.Stack.Hci.Opcodes;
 using WinBeacon.Stack.Hci.Parameters;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+/*
+0x00 Connectable undirected advertising (ADV_IND) (default)
+0x01 Connectable high duty cycle directed advertising (ADV_DIRECT_IND, high duty cycle)
+0x02 Scannable undirected advertising(ADV_SCAN_IND)
+0x03 Non connectable undirected advertising(ADV_NONCONN_IND)
+*/
 
 namespace WinBeacon.Stack.Hci.Commands
 {
     internal class LeSetAdvertisingParameters : Command
     {
-        public LeSetAdvertisingParameters(byte[] data)
-            : base(OpcodeGroup.LeController, (int)LeControllerOpcode.SetAdvertisingParameters )
+        /// <summary>
+        /// Set the advertising parameters
+        /// </summary>
+        /// <param name="advertisingIntervalMinInMs">Minimum advertising interval for undirected and low duty cycle directed advertising. Range: 20-10240  Default: 1280 = 1.28 seconds</param>
+        /// <param name="advertisingIntervalMaxInMs">Maximum advertising interval for undirected and low duty cycle directed advertising. Range: 20-10240  Default: 1280 = 1.28 seconds</param>
+        /// <param name="advertisingType">The Advertising_Type is used to determine the packet type that is used for advertising when advertising is enabled.</param>
+        /// <param name="ownAdressType"></param>
+        /// <param name="peerAdressType"></param>
+        /// <param name="peerAddress"></param>
+        /// <param name="advertisingChannelMap"></param>
+        /// <param name="advertisingFilterPolicy"></param>
+        public LeSetAdvertisingParameters(ushort advertisingIntervalMinInMs, ushort advertisingIntervalMaxInMs, AdvertisingType advertisingType, OwnAddressType ownAdressType, PeerAddressType peerAdressType, string peerAddress, AdvertisingChannelMap advertisingChannelMap, AdvertisingFilterPolicy advertisingFilterPolicy)
+            : base(OpcodeGroup.LeController, (int)LeControllerOpcode.SetAdvertisingParameters)
         {
-            if (data.Length > 15)
-                throw new ArgumentOutOfRangeException();
-            Parameters.Add(new ByteArrayCommandParameter(data));
+            ushort advertisingIntervalMin = (ushort)Math.Ceiling(advertisingIntervalMinInMs / 0.625);
+            ushort advertisingIntervalMax = (ushort)Math.Ceiling(advertisingIntervalMaxInMs / 0.625);
+            Debug.Assert(advertisingIntervalMin >= 0x0020);
+            Debug.Assert(advertisingIntervalMin <= 0x4000);
+            Debug.Assert(advertisingIntervalMax >= 0x0020);
+            Debug.Assert(advertisingIntervalMax <= 0x4000);
+            Parameters.Add(new UshortCommandParameter(advertisingIntervalMin));
+            Parameters.Add(new UshortCommandParameter(advertisingIntervalMax));
+            Parameters.Add(new UshortCommandParameter((byte)advertisingType));
+            Parameters.Add(new ByteArrayCommandParameter((from Match m in Regex.Matches(peerAddress, @"[0-9a-f]{2}", RegexOptions.IgnoreCase) select Convert.ToByte(m.Value, 16)).ToArray()));
+            Parameters.Add(new ByteCommandParameter((byte)advertisingChannelMap));
+            Parameters.Add(new ByteCommandParameter((byte)advertisingFilterPolicy));
         }
     }
 }
