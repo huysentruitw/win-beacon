c73ad24b5d20b9071655aa715894c12831a123c9
TheScribbler <stefan.magerstedt@khs.com>
2016-07-29 17:41:31 +0200

    Inserted Sample code for decreasing the advertising interval

--- samples/SampleConsole/Program.cs
+++ samples/SampleConsole/Program.cs
@@ -10,7 +10,7 @@ namespace SampleConsole
             using (var hub = new BeaconHub(0x0A5C, 0x21E8))
             {
                 hub.BeaconDetected += (sender, e) => Console.WriteLine(e.Beacon.ToString());
-                
+                hub.StoreAdvertisingParameters(200, 200, AdvertisingType.ADV_IND, OwnAddressType.Public, PeerAddressType.Public, "000000000000", AdvertisingChannelMap.ChannelAll, AdvertisingFilterPolicy.ConnectAllScanAll);
                 hub.EnableAdvertising(new Beacon("B9407F30-F5F8-466E-AFF9-25556B57FE6D", 1000, 2000, -52));
                 
                 Console.ReadKey();
