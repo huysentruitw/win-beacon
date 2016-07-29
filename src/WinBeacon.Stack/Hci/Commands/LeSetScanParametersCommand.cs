403516b5f747e1ea75e76c8bf5a5e915b330f14a
TheScribbler <stefan.magerstedt@khs.com>
2016-07-29 15:47:54 +0200

    It's not a multiplication but a division. 100ms / 0.625 = 160 = 0xA0
    N = 0xXXXX
    This is defined as the time interval from when the Controller started its last LE scan until it begins the subsequent LE scan.
    Range: 0x0004 to 0x4000
    Default: 0x0010 (10 ms)
    Time = N * 0.625 msec
    Time Range: 2.5 msec to 10.24 seconds

--- src/WinBeacon.Stack/Hci/Commands/LeSetScanParametersCommand.cs
+++ src/WinBeacon.Stack/Hci/Commands/LeSetScanParametersCommand.cs
@@ -26,8 +26,8 @@ namespace WinBeacon.Stack.Hci.Commands
         public LeSetScanParametersCommand(bool activeScanning, ushort intervalInMs, ushort windowInMs, bool useRandomAddress, bool onlyAcceptWhitelistedAdvertisers)
             : base(OpcodeGroup.LeController, (int)LeControllerOpcode.SetScanParameters)
         {
-            ushort intervalCode = (ushort)Math.Ceiling(intervalInMs * 0.625);
-            ushort windowCode = (ushort)Math.Ceiling(windowInMs * 0.625);
+            ushort intervalCode = (ushort)Math.Ceiling(intervalInMs / 0.625);
+            ushort windowCode = (ushort)Math.Ceiling(windowInMs / 0.625);
             Debug.Assert(intervalCode >= 0x0004);
             Debug.Assert(intervalCode <= 0x4000);
             Debug.Assert(windowCode >= 0x0004);
