05157823ba9357c598fe8d98a01f838977d4f8e9
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-06-02 20:40:52 +0200

    Add placeholder classes for Eddystone beacon frame types

---
+++ src/WinBeacon/Eddystone.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Copyright 2015-2019 Huysentruit Wouter
+ *
+ * See LICENSE file.
+ */
+
+namespace WinBeacon
+{
+    /// <summary>
+    /// Abstract base class that represents an Eddystone Beacon.
+    /// </summary>
+    public abstract class Eddystone
+    {
+    }
+
+    /// <summary>
+    /// Class that represents an Eddystone Beacon sending Eddystone-UID frames.
+    /// </summary>
+    public sealed class EddystoneUid : Eddystone
+    {
+    }
+
+    /// <summary>
+    /// Class that represents an Eddystone Beacon sending Eddystone-URL frames.
+    /// </summary>
+    public sealed class EddystoneUrl : Eddystone
+    {
+    }
+
+    /// <summary>
+    /// Class that represents an Eddystone Beacon sending Eddystone-TLM frames.
+    /// </summary>
+    public sealed class EddystoneTlm : Eddystone
+    {
+    }
+
+    /// <summary>
+    /// Class that represents an Eddystone Beacon sending Eddystone-EID frames.
+    /// </summary>
+    public sealed class EddystoneEid : Eddystone
+    {
+    }
+}
